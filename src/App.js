87eaf65769aa27177fbe42e1c2f18df2afb7d157
batukar <52932122+batukar@users.noreply.github.com>
2021-05-03 16:28:06 +0300

    edited

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return (
-    <div className="max-w-7xl">
-      <h1 className="bg-primary-dark">App</h1>
-    </div>
-  );
+    return (
+        <div className="max-w-7xl">
+            <h1 className="bg-primary-dark">App</h1>
+            <h1 className="bg-primary-dark">App</h1>
+        </div>
+    );
 };
 
 export default App;
