3535a14d2a401b6fbc91cc9f166c6ddf3cdd40db
batukar <52932122+batukar@users.noreply.github.com>
2021-05-05 00:54:03 +0300

    edited

--- src/Components/SideLink.js
+++ src/Components/SideLink.js
@@ -7,13 +7,11 @@ const SideLink = ({name, Icon, active, onMenuItemClick}) => {
             <a href={name.toLowerCase()} className='block cursor-pointer text-xl mb-2 pointer-events-none'>
                 <div className='inline-block'>
                     <div className={`flex items-center group-hover:bg-primary-light group-hover:text-primary-base rounded-full pl-3 pr-8 py-3
-                        ${isActive ? 'text-primary-base' : ''}
-                        `}>
-
+                        ${isActive ? 'text-primary-base' : ''}`}>
                         <Icon/>
                         <span className='ml-4 font-bold'>
-                        {name}
-                    </span>
+                            {name}
+                        </span>
                     </div>
                 </div>
             </a>
